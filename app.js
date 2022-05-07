6f4425f4990861c01975d87e0485f77c9cfa6702
gaetansbaffi <gaetansbaffi@live.fr>
2022-05-07 15:36:11 +0200

    Added math logic all calculations are working inside the console, + - * and / are supported. Only integer are supported

--- app.js
+++ app.js
@@ -18,7 +18,58 @@ function divide(a, b) {
 }
 
 function operate(operator, numberOne, numberTwo) {
-  return operator(numberOne, numberTwo);
+  if (operator == "multiply") {
+    return multiply(numberOne, numberTwo);
+  } else if (operator == "add") {
+    return add(numberOne, numberTwo);
+  } else if (operator == "substract") {
+    return substract(numberOne, numberTwo);
+  } else if (operator == "divide") {
+    return divide(numberOne, numberTwo);
+  }
 }
 
-//UI
+//selectors
+let btns = container.querySelectorAll(".btn");
+let tempOne = "";
+let tempOp = "";
+let tempTwo = "";
+let operation = { numberOne: "", operation: "", numberTwo: "" };
+
+btns.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    let choice = e.target.id;
+
+    if (!isNaN(parseInt(choice)) && !operation.operation) {
+      tempOne += choice;
+      operation.numberOne = tempOne;
+      console.log(operation);
+      return tempOne;
+    } else if (!isNaN(parseInt(choice)) && operation.operation) {
+      tempTwo += choice;
+      operation.numberTwo = tempTwo;
+      console.log(operation);
+      return tempTwo;
+    } else if (isNaN(parseInt(choice)) && choice !== "equal") {
+      operation.operation = choice;
+      console.log(operation);
+      return tempOp;
+    } else if (choice == "equal") {
+      let result = operate(
+        operation.operation,
+        parseInt(operation.numberOne),
+        parseInt(operation.numberTwo)
+      );
+      tempOne = result.toString();
+      tempTwo = "";
+      operation = {
+        numberOne: result.toString(),
+        operation: "",
+        numberTwo: "",
+      };
+      console.log(result);
+      console.log(operation);
+      return result;
+    }
+  })
+);
