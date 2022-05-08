73469af134b3702c59a6829c18040628986acbba
gaetansbaffi <gaetansbaffi@live.fr>
2022-05-08 16:20:29 +0200

    Added screen to the calculator fixed some bugs, cleaned code a bit, improved design

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //variables
 const container = document.querySelector(".container");
+const screenText = container.querySelector(".screenText");
 
 // functions
 
@@ -17,6 +18,10 @@ function divide(a, b) {
   return a / b;
 }
 
+function isInt(n) {
+  return n % 1 === 0;
+}
+
 function operate(operator, numberOne, numberTwo) {
   if (operator == "multiply") {
     return multiply(numberOne, numberTwo);
@@ -31,45 +36,50 @@ function operate(operator, numberOne, numberTwo) {
 
 //selectors
 let btns = container.querySelectorAll(".btn");
-let tempOne = "";
-let tempOp = "";
-let tempTwo = "";
 let operation = { numberOne: "", operation: "", numberTwo: "" };
 
-btns.forEach((btn) =>
-  btn.addEventListener("click", (e) => {
-    let choice = e.target.id;
+function doMaths(e) {
+  let choice = e.target.id;
 
-    if (!isNaN(parseInt(choice)) && !operation.operation) {
-      tempOne += choice;
-      operation.numberOne = tempOne;
-      console.log(operation);
-      return tempOne;
-    } else if (!isNaN(parseInt(choice)) && operation.operation) {
-      tempTwo += choice;
-      operation.numberTwo = tempTwo;
-      console.log(operation);
-      return tempTwo;
-    } else if (isNaN(parseInt(choice)) && choice !== "equal") {
-      operation.operation = choice;
-      console.log(operation);
-      return tempOp;
-    } else if (choice == "equal") {
-      let result = operate(
-        operation.operation,
-        parseInt(operation.numberOne),
-        parseInt(operation.numberTwo)
-      );
-      tempOne = result.toString();
-      tempTwo = "";
-      operation = {
-        numberOne: result.toString(),
-        operation: "",
-        numberTwo: "",
-      };
-      console.log(result);
-      console.log(operation);
-      return result;
+  if (!isNaN(parseInt(choice)) && !operation.operation) {
+    operation.numberOne += choice;
+    screenText.textContent = operation.numberOne;
+    return operation.numberOne;
+  } else if (!isNaN(parseInt(choice)) && operation.operation) {
+    operation.numberTwo += choice;
+    screenText.textContent = operation.numberTwo;
+    return operation.numberTwo;
+  } else if (
+    isNaN(parseInt(choice)) &&
+    choice !== "equal" &&
+    operation.numberOne
+  ) {
+    operation.operation = choice;
+    if (choice == "multiply") {
+      screenText.textContent = "*";
+    } else if (choice == "add") {
+      screenText.textContent = "+";
+    } else if (choice == "substract") {
+      screenText.textContent = "-";
+    } else if (choice == "divide") {
+      screenText.textContent = "/";
     }
-  })
-);
+    return operation.operation;
+  } else if (choice == "equal") {
+    let result = operate(
+      operation.operation,
+      parseInt(operation.numberOne).toFixed(2),
+      parseInt(operation.numberTwo).toFixed(2)
+    );
+    operation = {
+      numberOne: result.toString(),
+      operation: "",
+      numberTwo: "",
+    };
+
+    screenText.textContent = isInt(result) ? result : result.toFixed(2);
+    return result;
+  }
+}
+
+btns.forEach((btn) => btn.addEventListener("click", doMaths));
